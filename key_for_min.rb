86cf582c1d27f107435a666aa0cfa9b2f68d7c93
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-28 21:25:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 def key_for_min_value(hash)
   lowest_key = nil
-  lowest_value = nil
+  lowest_value = 0
   hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
       lowest_value = v
